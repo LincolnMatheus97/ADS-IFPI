120e73e3b212b7b329ed845c46f424f3c41c906e
Lincoln Matheus <149435974+LincolnMatheus97@users.noreply.github.com>
2024-03-27 21:27:26 -0300

    Algoritmo_Programacao

--- ExcAlgoritmoProgramacao_JS/Lista_3/L/AlgEx3Q11.js
+++ ExcAlgoritmoProgramacao_JS/Lista_3/L/AlgEx3Q11.js
@@ -29,9 +29,15 @@ function Entre_Limites(inferior, superior) {
 }
 
 function Eh_primo(num) {
-    if (num <= 1 || num % 2 === 0 ||
-        num % 3 === 0 || num % 5 === 0 ||
-        num % 7 === 0) {
+    if (num <= 1) {
+        return false;
+    } else if (num === 2) {
+        return true;
+    } else if (num % 2 === 0) {
+        return false;
+    } else if (num === 3 || num === 5 || num === 7) {
+        return true;
+    } else if (num % 3 === 0 || num % 5 === 0 || num % 7 === 0) {
         return false;
     } else {
         return true;
