4e6655295a6870abfc266c89f31ba68c3d2ad278
Lincoln <linconlmatheus25@gmail.com>
2024-10-22 10:42:10 -0300

    Exercicios_ED_I

---
+++ Exercicios_ES_I/exercicios_Cap1/AtCap01_05.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3, b = 5;
+    int *p = &a, *q = &b;
+
+    *p = *p + *q;
+    *q = *p - *q;
+    *p = *p - *q;
+
+    printf("%d %d\n", a , b);
+}
---
+++ Exercicios_ES_I/exercicios_Cap1/modelo1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+typedef struct {
+	int mat;
+	char nome[30];
+	} TAluno;
+TAluno v[20];
+int ultimo = 0;
+void inserir(TAluno novo){	
+}
+void remover(int procurado){	
+}
+void mostra(){	
+}
+main(){
+	
+}
