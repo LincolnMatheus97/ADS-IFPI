f9b8183afa5cfc44f3ef3e65c7a3e1931f67327c
Lincoln <linconlmatheus25@gmail.com>
2025-01-07 08:14:26 -0300

    ExercicioED

---
+++ Exercicios_ED_I/Exercicios_Extras/atividadeAvaliativa/Q4.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Q4.2.h"
+
+int main() {
+    LISTA listinha = criarLista(10);
+    inserirNoInicio(20, &listinha);
+    inserirNoInicio(30, &listinha);
+    inserirNoFinal(40, &listinha);
+    mostrarInicioAoFim(listinha);
+    printf("\n");
+    mostrarFimAoInicio(listinha);
+    printf("\n");
+}
---
+++ Exercicios_ED_I/Exercicios_Extras/atividadeAvaliativa/Q4.2.h
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int intItem;
+
+struct noDuplo {
+    intItem valor;
+    struct noDuplo *prox;
+    struct noDuplo *ant;
+};
+
+typedef struct noDuplo* LISTA;
+
+LISTA criarLista(intItem x) {
+    LISTA listinha = (LISTA)malloc(sizeof(struct noDuplo));
+    listinha->valor = x;
+    listinha->prox = NULL;
+    listinha->ant = NULL;
+    return listinha;
+}
+
+void inserirNoInicio(intItem x, LISTA *l) {
+    LISTA novoNo = (LISTA)malloc(sizeof(struct noDuplo));
+    novoNo->valor = x;
+    novoNo->prox = *l;
+    novoNo->ant = NULL;
+    if (*l != NULL) {
+        (*l)->ant = novoNo;
+    }
+    *l = novoNo;
+}
+
+void inserirNoFinal(intItem x, LISTA *l) {
+    LISTA novoNo = (LISTA)malloc(sizeof(struct noDuplo));
+    novoNo->valor = x;
+    novoNo->prox = NULL;
+    novoNo->ant = NULL;
+
+    if (*l == NULL) {
+        *l = novoNo;
+    } else {
+        LISTA aux = *l;
+        while (aux->prox != NULL) {
+            aux = aux->prox;
+        }
+        aux->prox = novoNo;
+        novoNo->ant = aux;
+    }
+}
+
+void removerItemEspecifico(intItem x, LISTA *l) {
+    if (*l == NULL) {
+        return;
+    }
+    LISTA ant, novoNo = *l;
+    while (novoNo != NULL && novoNo->valor != x) {
+        ant = novoNo;
+        novoNo = novoNo->prox;
+    }
+    if (novoNo == NULL) {
+        return;
+    }
+    if (novoNo == *l) {
+        *l = novoNo->prox;
+    } else {
+        ant->prox = novoNo->prox;
+    }
+    free(novoNo);
+}
+
+void mostrarInicioAoFim(LISTA l) {
+    while (l != NULL) {
+        printf("%d ", l->valor);
+        l = l->prox;
+    }
+}
+
+void mostrarFimAoInicio(LISTA l) {
+    if (l == NULL) {
+        return;
+    }
+    while (l->prox != NULL) {
+        l = l->prox;
+    }
+    while (l != NULL) {
+        printf("%d ", l->valor);
+        l = l->ant;
+    }
+}
