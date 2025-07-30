24190134c18701e0060186b80702d20e13fad5e2
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-07-30 20:28:56 -0300

    :sparkles: feat: Adiciona solução da questão 2839 do bee

---
+++ ExcAlgoritmoProgramacao_JS/Lista_Bee/2839_matematica.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int pares;
+    scanf("%d", &pares);
+
+    int qtd_min_meias = pares + 1;
+    printf("%d\n", qtd_min_meias);
+    return 0;
+}
