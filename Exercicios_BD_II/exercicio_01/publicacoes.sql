872b101a39846d777594aecf5dd67c0941c0d750
LincolnMatheus97 <linconlmatheus25@gmail.com>
2026-02-19 23:20:39 -0300

    :sparkles: feat: Adiciona novas consultas

--- Exercicios_BD_II/exercicio_01/publicacoes.sql
+++ Exercicios_BD_II/exercicio_01/publicacoes.sql
@@ -236,7 +236,7 @@ GROUP BY L.titulo, E.nome, A.descricao;
 SELECT E.nome AS editora, L.titulo AS titulo
 FROM livro L INNER JOIN editora E ON L.editora_codigo = E.codigo
 ORDER BY E.nome ASC, L.titulo ASC;
--- Editoras cadastradas e para aquelas que possuem livros publicados, relacionar também o título do livro, em ordem de nome da editora e pelo título do livro.
+-- Editoras cadastradas e para aquelas que possuem livros publicados, relacionar também o título do livro, em ordem de nome da editora e pelo título do livro
 SELECT E.nome AS editora, L.titulo AS titulo
 FROM editora E LEFT JOIN livro L ON E.codigo = L.editora_codigo
 ORDER BY E.nome ASC, L.titulo ASC;
@@ -254,3 +254,91 @@ FROM assunto A LEFT JOIN livro L ON A.codigo = L.assunto_codigo;
 SELECT A.nome AS autor, L.titulo AS autoria
 FROM autor_livro AL INNER JOIN autor A INNER JOIN livro L ON AL.autor_codigo = A.codigo AND AL.livro_codigo = L.codigo
 ORDER BY A.nome ASC;
+--  Editoras que publicaram livros escritos pelo autor ‘Machado de Assis’
+SELECT E.nome AS editora
+FROM autor_livro AL INNER JOIN autor A INNER JOIN livro L INNER JOIN editora E ON AL.autor_codigo = A.codigo AND AL.livro_codigo = L.codigo 
+AND L.editora_codigo = E.codigo WHERE A.nome LIKE "%Machado de Assis%";
+-- Quantidade de livros lançados que foram escritos por um autor cujo nome possui a palavra ‘Luis’
+SELECT COUNT(DISTINCT L.codigo) AS qnt_livros
+FROM autor A INNER JOIN autor_livro AL ON A.codigo = AL.autor_codigo
+INNER JOIN livro L ON L.codigo = AL.livro_codigo
+WHERE A.nome LIKE "%Luis%" AND L.dt_lancamento IS NOT NULL;
+-- O preço do livro mais caro publicado pela editora ‘Books Editora’ sobre banco de dados
+SELECT MAX(L.preco) AS mais_caro
+FROM livro L INNER JOIN editora E ON L.editora_codigo = E.codigo
+INNER JOIN assunto A ON L.assunto_codigo = A.codigo
+WHERE E.nome LIKE "%Books Editora%" AND A.descricao LIKE "%Banco de Dados%";
+-- Editoras que não publicaram livros.
+SELECT E.nome AS editoras
+FROM livro L RIGHT JOIN editora E ON L.editora_codigo = E.codigo
+WHERE L.codigo IS NULL;
+--  Título do livro e o nome da editora que o publica para todos os livros que custam menos que R$ 50,00
+SELECT L.titulo AS titulo, E.nome AS editora 
+FROM livro L INNER JOIN editora E ON L.editora_codigo = E.codigo
+WHERE L.preco < 50.0;
+--  Nome e passaporte do autor brasileiro que tenha nascido antes de 1° de janeiro de 1950 e os títulos dos livros de sua autoria, ordenado pelo nome do autor e pelo título do livro
+SELECT A.nome AS nome_autor, A.passaporte AS passaporte, L.titulo AS livro
+FROM autor A INNER JOIN autor_livro AL ON AL.autor_codigo = A.codigo
+INNER JOIN livro L ON AL.livro_codigo = L.codigo
+WHERE A.passaporte LIKE "BRA%" AND A.dt_nascimento < "1950-01-01" ORDER BY A.nome ASC;
+-- Nome e passaporte do autor e o preço máximo dos livros de sua autoria
+SELECT A.nome AS nome_autor, A.passaporte AS passaporte, MAX(L.preco) AS livro
+FROM autor A INNER JOIN autor_livro AL ON AL.autor_codigo = A.codigo
+INNER JOIN livro L ON AL.livro_codigo = L.codigo GROUP BY A.nome, A.passaporte;
+-- Nome do autor e nome da editora que já lançaram pelo menos 2 livros
+SELECT A.nome AS nome_autor, E.nome AS editora
+FROM autor A INNER JOIN autor_livro AL ON AL.autor_codigo = A.codigo
+INNER JOIN livro L ON AL.livro_codigo = L.codigo 
+INNER JOIN editora E ON E.codigo = L.editora_codigo
+GROUP BY A.nome, E.nome HAVING COUNT(L.codigo) >= 2;
+-- Descrição do assunto referenciado em pelo menos 10 livros
+SELECT A.descricao AS descricao
+FROM livro L INNER JOIN assunto A ON L.assunto_codigo = A.codigo
+GROUP BY A.descricao HAVING COUNT(L.codigo) >= 10;
+-- Assuntos não foram lançados livros
+SELECT A.descricao AS assunto
+FROM livro L RIGHT JOIN assunto A ON L.assunto_codigo = A.codigo
+WHERE L.codigo IS NULL;
+-- Descrição dos assuntos e quantidade de livros lançados de cada um
+SELECT A.descricao AS descricao, COUNT(L.assunto_codigo) AS qnt_lancados
+FROM livro L INNER JOIN assunto A ON L.assunto_codigo = A.codigo
+GROUP BY A.descricao, L.assunto_codigo;
+-- Nome das editoras e o preço médio dos livros de cada uma
+SELECT E.nome AS editoras, AVG(L.preco) AS preco_medio
+FROM livro L INNER JOIN editora E ON L.editora_codigo = E.codigo
+GROUP BY E.nome;
+-- Nome das editoras e os livros das editoras que lançaram ao menos 2 livros, ordenados pelo nome da editora e pelo título da publicação
+SELECT E.nome AS editoras, L.titulo AS livros
+FROM livro L INNER JOIN editora E ON L.editora_codigo = E.codigo
+GROUP BY E.nome, L.titulo HAVING COUNT(L.codigo) <= 2 ORDER BY E.nome ASC;
+-- Títulos dos livros dos assuntos cujo preço médio do livro é superior a R$ 40,00, juntamente com os respectivos assuntos
+SELECT L.titulo AS titulo, A.descricao AS assunto
+FROM livro L INNER JOIN assunto A ON L.assunto_codigo = A.codigo
+WHERE L.assunto_codigo IN (
+SELECT assunto_codigo FROM livro
+GROUP BY assunto_codigo HAVING AVG(preco) >= 40.00
+);
+-- Títulos dos livros cujo assunto é ‘Banco de Dados’ ou que foram lançados por editoras que contenham 'Books’ no nome
+SELECT titulo FROM livro WHERE assunto_codigo IN 
+	(SELECT codigo FROM assunto WHERE descricao LIKE "%Banco de Dados%") 
+    OR editora_codigo IN
+    (SELECT codigo FROM editora WHERE nome LIKE "%Books%" );
+-- Títulos dos livros cujo assunto é ‘Banco de Dados’ e que foram lançados por editoras que contenham ‘Books’ no nome
+SELECT titulo FROM livro WHERE assunto_codigo IN 
+	(SELECT codigo FROM assunto WHERE descricao LIKE "%Banco de Dados%") 
+    AND editora_codigo IN
+    (SELECT codigo FROM editora WHERE nome LIKE "%Books%" );
+-- Títulos dos livros cujo assunto é ‘Banco de Dados’ e que não foram lançados por editoras que contenham ‘Books’ no nome
+SELECT titulo FROM livro WHERE assunto_codigo IN 
+	(SELECT codigo FROM assunto WHERE descricao LIKE "%Banco de Dados%")
+    AND editora_codigo NOT IN
+    (SELECT codigo FROM editora WHERE nome LIKE "%Books%" );
+-- Títulos dos livros que não foram lançados por editoras que contenham ‘Books’ no nome cujo assunto é ‘Banco de Dados’
+SELECT titulo FROM livro WHERE assunto_codigo NOT IN 
+	(SELECT codigo FROM assunto WHERE descricao LIKE "%Banco de Dados%")
+    AND editora_codigo IN
+    (SELECT codigo FROM editora WHERE nome LIKE "%Books%" );
+-- Excluir as editoras que não publicaram livros
+DELETE FROM editora WHERE codigo NOT IN (
+	SELECT DISTINCT editora_codigo FROM livro WHERE editora_codigo IS NULL
+);
